02a24ede1000efad6019ff47f7b46343983b6e84
Felix Kettnaker <felix.kettnaker@batix.com>
2025-10-21 09:45:11 +0200

    astrocommunity + language bundles

--- lua/community.lua
+++ lua/community.lua
@@ -1,12 +1,20 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
--- AstroCommunity: import any community modules here
--- We import this file in `lazy_setup.lua` before the `plugins/` folder.
--- This guarantees that the specs are processed before any user plugins.
-
 ---@type LazySpec
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
   -- import/override with your plugins folder
+  -- language packs
+  { import = "astrocommunity.pack.yaml" },
+  { import = "astrocommunity.pack.toml" },
+  { import = "astrocommunity.pack.xml" },
+  { import = "astrocommunity.pack.html-css" },
+  { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.pack.vue" },
+  { import = "astrocommunity.pack.java" },
+  { import = "astrocommunity.pack.kotlin" },
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.godot" },
+
+  -- functional
+  { import = "astrocommunity.motion.nvim-surround" },
 }
