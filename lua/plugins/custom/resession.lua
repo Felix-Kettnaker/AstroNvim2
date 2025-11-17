65a2a7979f9828c84e05626ca60648c5c475e1ab
Felix Kettnaker <felix.kettnaker@batix.com>
2025-11-17 15:44:39 +0100

    resession mapping unifications, save dirsession just as top dir name

--- lua/plugins/custom/resession.lua
+++ lua/plugins/custom/resession.lua
@@ -20,22 +20,36 @@ return {
         maps.n["<C-s>"] = { desc = vim.tbl_get(opts, "_map_sections", "<c-s>") }
         maps.n["<C-s>l"] = { function() require("resession").load "Last Session" end, desc = "Load last session" }
         maps.n["<C-s>s"] = { function() require("resession").save() end, desc = "Save this session" }
-        maps.n["<C-s>S"] = {
-          function() require("resession").save(vim.fn.getcwd(), { dir = "dirsession" }) end,
+        maps.n["<C-s><C-s>"] = {
+          function() require("resession").save(vim.fn.getcwd():match ".*/(.*)$", { dir = "dirsession" }) end,
           desc = "Save this dirsession",
         }
         maps.n["<C-s>t"] = { function() require("resession").save_tab() end, desc = "Save this tab's session" }
         maps.n["<C-s>d"] = { function() require("resession").delete() end, desc = "Delete a session" }
-        maps.n["<C-s>D"] =
+        maps.n["<C-s><C-d>"] =
           { function() require("resession").delete(nil, { dir = "dirsession" }) end, desc = "Delete a dirsession" }
         maps.n["<C-s>f"] = { function() require("resession").load() end, desc = "Load a session" }
-        maps.n["<C-s>F"] =
-          { function() require("resession").load(nil, { dir = "dirsession" }) end, desc = "Load a dirsession" }
         maps.n["<C-s><C-f>"] =
           { function() require("resession").load(nil, { dir = "dirsession" }) end, desc = "Load a dirsession" }
         maps.n["<C-s>."] = {
-          function() require("resession").load(vim.fn.getcwd(), { dir = "dirsession" }) end,
+          function() require("resession").load(vim.fn.getcwd():match ".*/(.*)$", { dir = "dirsession" }) end,
           desc = "Load current dirsession",
+        }
+
+        opts.autocmds.resession_auto_save = {
+          {
+            event = "VimLeavePre",
+            desc = "Save session on close",
+            callback = function()
+              local buf_utils = require "astrocore.buffer"
+              local autosave = require("astrocore").config.sessions.autosave
+              if autosave and buf_utils.is_valid_session() then
+                local save = require("resession").save
+                if autosave.last then save("Last Session", { notify = false }) end
+                if autosave.cwd then save(vim.fn.getcwd():match ".*/(.*)$", { dir = "dirsession", notify = false }) end
+              end
+            end,
+          },
         }
       end,
     },
