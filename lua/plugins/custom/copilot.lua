91770135780aa76919a4a7fdccebd210e085af55
Felix Kettnaker <felix.kettnaker@batix.com>
2025-12-10 13:10:56 +0100

    copilot

---
+++ lua/plugins/custom/copilot.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "zbirenbaum/copilot.lua",
+    opts = {
+      suggestion = {
+        auto_trigger = true,
+        keymap = {
+          accept = "<C-D-Space>",
+          accept_word = "<D-Right>",
+          next = "<C-D-.>",
+          prev = "<C-D-,>",
+        },
+      },
+    },
+  },
+}
