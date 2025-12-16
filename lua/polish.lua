e513891b53ced99cac1cf3b76cdb03868b27ac6c
Felix Kettnaker <felix.kettnaker@batix.com>
2025-12-16 14:04:39 +0100

    disable "unnecessary code" diagnostic highlight
    I still want to have syntax highlighting in those sections

--- lua/polish.lua
+++ lua/polish.lua
@@ -1,5 +1,6 @@
-if true then return end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- This will run last in the setup process.
 -- This is just pure lua so anything that doesn't
 -- fit in the normal config locations above can go here
+
+-- disale graying out "unnecessary" code (sometimes entire functions)
+vim.api.nvim_set_hl(0, "DiagnosticUnnecessary", { link = "NONE" })
