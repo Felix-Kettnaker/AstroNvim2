f81de34863f10822425c2dbfef7643571189c7e3
Felix Kettnaker <felix.kettnaker@batix.com>
2026-05-15 17:32:14 +0200

    treesj ternary support

--- lua/plugins/custom/treesj.lua
+++ lua/plugins/custom/treesj.lua
@@ -1,9 +1,33 @@
 return {
   "Wansmer/treesj",
-  opts = {
-    use_default_keymaps = false,
-    max_join_length = 160,
-  },
+  opts = function(_, opts)
+    opts.use_default_keymaps = false
+    opts.max_join_length = 160
+
+    local utils = require('treesj.langs.utils')
+    local ternary = utils.set_preset_for_non_bracket({
+      both = {
+        omit = {
+          function(tsj)
+            local prev = tsj:tsnode():prev_sibling()
+            -- first child (condition) -> stay on the opening line
+            if prev == nil then return true end
+            -- consequence / alternative -> stay attached to ? or :
+            local t = prev:type()
+            return t == '?' or t == ':'
+          end,
+        },
+      },
+      split = {
+        last_indent = 'inner',
+      },
+    })
+    opts.langs = vim.tbl_deep_extend('force', opts.langs or {}, {
+      javascript = { ternary_expression = ternary },
+      typescript = { ternary_expression = ternary },
+      tsx        = { ternary_expression = ternary },
+    })
+  end,
   specs = {
     {
       "AstroNvim/astrocore",
