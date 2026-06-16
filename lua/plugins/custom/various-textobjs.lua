3fb29945473f2785b4a2997beb72341fe134b208
Felix Kettnaker <felix.kettnaker@batix.com>
2026-06-16 15:56:24 +0200

    Number (Zahl) Textobject

--- lua/plugins/custom/various-textobjs.lua
+++ lua/plugins/custom/various-textobjs.lua
@@ -3,5 +3,7 @@ return {
   keys = {
     { "i<M-w>", function() require("various-textobjs").subword "inner" end, mode = { "x", "o" }, desc = "Inner subword" },
     { "a<M-w>", function() require("various-textobjs").subword "outer" end, mode = { "x", "o" }, desc = "Around subword" },
+    { "iz", function() require("various-textobjs").number "inner" end, mode = { "x", "o" }, desc = "Inner number (Zahl)" },
+    { "az", function() require("various-textobjs").number "outer" end, mode = { "x", "o" }, desc = "Outer number (Zahl)" },
   },
 }
